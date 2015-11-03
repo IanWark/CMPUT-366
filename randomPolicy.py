b1c614c371f37d43553f76d6c55c0082b455895e
IanWark <wark@ualberta.ca>
2015-11-02 17:58:14 -0700

    r

--- randomPolicy.py
+++ randomPolicy.py
@@ -1,12 +1,30 @@
 import blackjack
-from pylab import *
+import numpy
+import random
 
+#from scipy import *
 numEpisodes = 2000
+
+
+def showOneGame():
+    s=blackjack.init()
+
+    # this is a defined set of moves, we need to make (equiprobably)
+    # randomly choose a move.
+    turn=0
+    total_return = 0
+    while s!=-1: #-1 is terminal
+        a = numpy.random.randint(0, 2)
+        r,sp=blackjack.sample(s,a)
+        total_return += r
+        s=sp
+        turn+=1
+    return total_return
 
 returnSum = 0.0
 for episodeNum in range(numEpisodes):
     G = 0
-    ...
-    print "Episode: ", episodeNum, "Return: ", G
+    G = showOneGame();
+    print("Episode: ", episodeNum, "Return: ", G)
     returnSum = returnSum + G
-print "Average return: ", returnSum/numEpisodes
+print("Average return: ", returnSum/numEpisodes)
