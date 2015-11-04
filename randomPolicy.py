748bbe96afca2941d109d47a867a4601ec97dad3
IanWark <wark@ualberta.ca>
2015-11-03 17:11:40 -0700

    ExpectedSarsa

--- randomPolicy.py
+++ randomPolicy.py
@@ -11,19 +11,16 @@ def showOneGame():
 
     # this is a defined set of moves, we need to make (equiprobably)
     # randomly choose a move.
-    turn=0
     total_return = 0
     while s!=-1: #-1 is terminal
         a = numpy.random.randint(0, 2)
         r,sp=blackjack.sample(s,a)
         total_return += r
         s=sp
-        turn+=1
     return total_return
 
 returnSum = 0.0
 for episodeNum in range(numEpisodes):
-    G = 0
     G = showOneGame();
     print("Episode: ", episodeNum, "Return: ", G)
     returnSum = returnSum + G
