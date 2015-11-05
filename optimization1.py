4b3d31efbe4c92f33acabe18ac345a946e5beeb9
IanWark <wark@ualberta.ca>
2015-11-04 18:49:14 -0700

    push

---
+++ optimization1.py
@@ -0,0 +1,36 @@
+import blackjack
+import numpy
+import random
+import ExpectedSarsa
+
+alpha = 0.001
+Epi = 0.01
+Emu = 0.01
+numEpisodes = 1000000
+
+maxQ = []
+maxReturn = -1
+maxEpi=0
+maxEmu=0
+maxAlpha=0
+
+for alpha in [0.1,0.01]:
+    for Epi in [0.4,0.35,0.03,0.25,0.02,0.15,0.01,0.5,0.25,0.125,0.0625]:
+        for Emu in [0.4,0.35,0.03,0.25,0.02,0.15,0.01,0.5,0.25,0.125,0.0625]:
+            print('alpha:',alpha,'Epi:',Epi,'Emu',Emu)
+            Q,averageReturn = ExpectedSarsa.ExpectedSarsa(alpha,Epi,Emu,numEpisodes)
+            print(averageReturn)
+            Q,averageReturn = ExpectedSarsa.ExpectedSarsa(0,0,0,numEpisodes,Q) 
+            print(averageReturn)
+            if averageReturn > maxReturn:
+                maxAlpha = alpha
+                maxEpi = Epi
+                maxEmu = Emu
+                maxQ = Q
+                maxReturn = averageReturn
+                
+                
+print(maxAlpha,maxEpi,maxEmu,maxReturn)
+f = open('optimization1.txt','w')
+f.write('maxAlpha: '+str(maxAlpha)+' maxEpi: '+str(maxEpi)+' maxEmu: '+str(maxEmu)+' maxReturn: '+str(maxReturn))
+f.close()
---
+++ randomPolicy.py
@@ -0,0 +1,27 @@
+import blackjack
+import numpy
+import random
+
+#from scipy import *
+numEpisodes = 2000
+
+
+def showOneGame():
+    s=blackjack.init()
+
+    # this is a defined set of moves, we need to make (equiprobably)
+    # randomly choose a move.
+    total_return = 0
+    while s!=-1: #-1 is terminal
+        a = numpy.random.randint(0, 2)
+        r,sp=blackjack.sample(s,a)
+        total_return += r
+        s=sp
+    return total_return
+
+returnSum = 0.0
+for episodeNum in range(numEpisodes):
+    G = showOneGame();
+    print("Episode: ", episodeNum, "Return: ", G)
+    returnSum = returnSum + G
+print("Average return: ", returnSum/numEpisodes)
