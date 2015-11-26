3e77ccc482aa66247020464f08de9a39bc378d4c
IanWark <wark@ualberta.ca>
2015-11-26 10:11:13 -0700

    p3

--- plot.py
+++ plot.py
@@ -2,6 +2,8 @@
 Created on Nov 17, 2011
 
 @author: rupam
+
+Invoke this with "python plot.py yourFileName"
 '''
 import sys
 from numpy import *
@@ -10,17 +12,17 @@ from mpl_toolkits.mplot3d import Axes3D
 
 def main():
     x1steps = x2steps = 50.0
-    x1range = linspace(0, 6-6/x1steps, x1steps)
-    x2range = linspace(0, 6-6/x2steps, x2steps)
+    x1range = linspace(-1.2, 0.5, x1steps)
+    x2range = linspace(-0.07, 0.07, x2steps)
     y = loadtxt(sys.argv[1])
 
     fig = figure()
     ax = Axes3D(fig)
     x1,x2 = meshgrid(x1range,x2range)
     ax.plot_surface(x1, x2, y.T, cstride=1, rstride=1, cmap=cm.jet)
-    ax.set_xlabel('$in_1$')
-    ax.set_ylabel('$in_2$')
-    ax.set_zlabel('$f$')
+    ax.set_xlabel('position')
+    ax.set_ylabel('velocity')
+    ax.set_zlabel('$-max_a Q(s,a)$')
     savefig("plot.pdf", bbox_inches='tight')
     
 if __name__ == '__main__':
